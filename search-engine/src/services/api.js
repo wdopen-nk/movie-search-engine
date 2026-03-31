585363fbddbf6d7ba77168f2880227f8483287fc
wdopen-nk <nihatsadigzada06@gmail.com>
2026-03-31 16:49:56 +0400

    Add API key and implement searchMovies, getPopularMovies methods

---
+++ search-engine/src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "53a3d1c985c5e509d2dc43faf80ace5a";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}
