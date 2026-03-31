597dba883008ab247956be079321cc0535a6fc7d
wdopen-nk <nihatsadigzada06@gmail.com>
2026-03-31 16:51:19 +0400

    Implement the favorite page

---
+++ search-engine/src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import '../css/Favorites.css';
+
+function Favorites() {
+    return (
+        <div className='favorites-empty'>
+            <h2 className='favorites'>No Favorite Movies Yet</h2>
+            <p>Start adding movies to your favorites!</p>
+        </div>
+    );
+}
+
+export default Favorites;
